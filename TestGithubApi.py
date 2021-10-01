07a28d7faaee34e807c1e6c190e9318e9868210b
Van Darkholme <37331560+0Trickster0@users.noreply.github.com>
2021-09-30 21:03:42 -0400

    change yml setting

--- TestGithubApi.py
+++ TestGithubApi.py
@@ -4,7 +4,7 @@ from GithubApi import *
 
 class GithubApiTest(unittest.TestCase):
     def test_parse_github_info(self):
-        right_str = ["Repo: 001, Number of commits: 0", "Repo: GitHubApi567, Number of commits: 4",
+        right_str = ["Repo: 001, Number of commits: 0", "Repo: GitHubApi567, Number of commits: 5",
                      "Repo: myTensorflow, Number of commits: 0", "Repo: So_We_Date, Number of commits: 15",
                      "Repo: SSW567WS, Number of commits: 8", "Repo: Steps, Number of commits: 7",
                      "Repo: Student-Repository, Number of commits: 6"]
